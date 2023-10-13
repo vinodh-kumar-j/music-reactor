8560de9a5f60a9c4492289bf8ad4fb889b138ef5
vinodh kumar j <74519939+vinodh-kumar-j@users.noreply.github.com>
2023-10-13 22:35:11 +0530

    Update music_reactor_11.ino

--- music_reactor_11.ino
+++ music_reactor_11.ino
@@ -1,86 +1,26 @@
-#include <esp_now.h>
-#include <WiFi.h>
+//Music Reactive Led Strip
+int soundSensor = 12;  //define Sound Sensor Pin
+int LED = 11;          //define LED Strip Pin
 
-// REPLACE WITH YOUR RECEIVER MAC Address
-uint8_t broadcastAddress[] = {0xA4, 0xCF, 0x12, 0xBF, 0xD3, 0xF3};
-
-// Structure example to send data
-// Must match the receiver structure
-typedef struct struct_message {
-  char a[32];
-  int b;
-  float c;
-  bool d;
-} struct_message;
-
-// Create a struct_message called myData
-struct_message myData;
-
-esp_now_peer_info_t peerInfo;
-
-// callback when data is sent
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  Serial.print("\r\nLast Packet Send Status:\t");
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
-}
- 
-void setup() {
-  // Init Serial Monitor
-  Serial.begin(115200);
- 
-  // Set device as a Wi-Fi Station
-  WiFi.mode(WIFI_STA);
-
-  // Init ESP-NOW
-  if (esp_now_init() != ESP_OK) {
-    Serial.println("Error initializing ESP-NOW");
-    return;
-  }
-  
-  // Once ESPNow is successfully Init, we will register for Send CB to
-  // get the status of Trasnmitted packet
-  esp_now_register_send_cb(OnDataSent);
-  
-  // Register peer
-  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 0;  
-  peerInfo.encrypt = false;
-  
-  // Add peer        
-  if (esp_now_add_peer(&peerInfo) != ESP_OK){
-    Serial.println("Failed to add peer");
-    return;
-  }
-}
- 
-void loop() {
-  // Set values to send
-  strcpy(myData.a, "THIS IS A CHAR");
-  myData.b = random(1,20);
-  myData.c = 1.2;
-  myData.d = false;
-  
-  // Send message via ESP-NOW
-  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
-   
-  if (result == ESP_OK) {
-    Serial.println("Sent with success");
-  }
-  else {
-    Serial.println("Error sending the data");
-  }
-  delay(2000);
+void setup() 
+{
+  pinMode (soundSensor, INPUT); //define Sound Sensor as input
+  pinMode (LED, OUTPUT);        //define LED Strip as output
 }
 
-/*void loop() {
-  myData = {"THIS IS A CHAR", random(1, 20), 1.2, false};
+void loop()
+{
+  int statusSensor = digitalRead (soundSensor);   //define variable of the Sound Sensor status
+                                                  //and read value of the Sensor's
   
-  if (esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData)) == ESP_OK) {
-    Serial.println("Sent with success");
-  } else {
-    Serial.println("Error sending the data");
+  if (statusSensor == 1)        //When the Sensor detects a signal
+  {
+    digitalWrite(LED, HIGH);    //LED Strip will be active
   }
   
-  delay(2000);
+  else                          //If no signal is detected
+  {
+    digitalWrite(LED, LOW);     //The status of the LED strip is deactivated
+  }
+  
 }
-*/
